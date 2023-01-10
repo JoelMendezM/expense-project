2146d5a4bb990e457a945579396e84070045c2a3
Joel Mendez <joel.j.mendez.m@gmail.com>
2023-01-09 23:07:01 -0300

    adding Chart components

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import CharBart from "./ChartBar";
+import "./Chart.css";
+
+const Chart = (props) => {
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <CharBart
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
