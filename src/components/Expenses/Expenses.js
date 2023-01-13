19671176a3126854591a0f6a8f45f95995ed7c39
Joel Mendez <joel.j.mendez.m@gmail.com>
2023-01-13 00:07:28 -0300

    fixing barFill bug

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,15 +6,15 @@ import ExpensesList from "./ExpensesList";
 import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
-  const [filteredYear, setYearSelected] = useState("2022");
+  const [filteredYear, setFilteredYear] = useState("2022");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
-
-  const filterChangeHandler = (selectYear) => {
-    setYearSelected(selectYear);
-  };
 
   return (
     <div>
